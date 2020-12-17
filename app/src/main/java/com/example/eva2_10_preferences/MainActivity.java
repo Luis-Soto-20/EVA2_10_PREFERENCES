17a41ca512bdd2260dfebccdc44c74dc38be210a
Luis-Soto-20 <60636096+Luis-Soto-20@users.noreply.github.com>
2020-12-16 19:53:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/eva2_10_preferences/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.eva2_10_preferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.settings_layout, new SettingsFragment());
+        ft.commit();
+    }
+}
